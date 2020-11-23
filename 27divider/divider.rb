2184cd88cc769358f2804d5b0e91c3c065c0cd30
stevenacz <scoailazaa@gmail.com>
2020-11-23 12:38:04 -0500

    Problema 27

--- 27divider/divider.rb
+++ 27divider/divider.rb
@@ -1,6 +1,16 @@
+# class ZeroDivisionError < StandardError; end
+class CantDivideBy100 < StandardError; end
+
 module Divider
   def self.division(number1, number2)
-    # modify this method
+    raise CantDivideBy100 unless number2 != 100
+
     number1 / number2
+  rescue CantDivideBy100
+    "I don't like to divide by 100"
+  rescue ZeroDivisionError
+    "I cannot divide by zero"
+  rescue StandardError
+    "Both arguments need to be Numerics"
   end
 end
